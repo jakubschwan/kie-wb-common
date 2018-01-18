d33dd82a5097f03676ec9010d0505dde0f1237e3
Massimiliano <desmax74@yahoo.it>
2018-01-18 12:12:16 +0100

    added header license and remove test impl

--- kie-wb-common-services/kie-wb-common-compiler/kie-wb-common-compiler-core/src/test/java/org/kie/workbench/common/services/backend/compiler/impl/classloader/MapClassLoaderTest.java
+++ kie-wb-common-services/kie-wb-common-compiler/kie-wb-common-compiler-core/src/test/java/org/kie/workbench/common/services/backend/compiler/impl/classloader/MapClassLoaderTest.java
@@ -1,64 +1,17 @@
 package org.kie.workbench.common.services.backend.compiler.impl.classloader;
 
-import org.drools.core.util.ClassUtils;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Collections;
+
 import org.junit.Test;
 
-
-import static org.junit.Assert.assertEquals;
-
 public class MapClassLoaderTest {
-    @Test
-    public void testCanonicalNameSimpleClass() {
-        String name = ClassUtils.canonicalName(MapClassLoaderTest.class );
-        assertEquals( "org.kie.workbench.common.services.backend.compiler.impl.classloader.MapClassLoaderTest",
-                      name );
-    }
 
     @Test
-    public void testCanonicalNameInnerClass() {
-        String name = ClassUtils.canonicalName( A.class );
-        assertEquals( "org.kie.workbench.common.services.backend.compiler.impl.classloader.MapClassLoaderTest.A",
-                      name );
-    }
+    public void test(){
+        MapClassLoader EMPTY_CLASSLOADER = new MapClassLoader(Collections.emptyMap(), new URLClassLoader(new URL[0], ClassLoader.getSystemClassLoader().getParent()));
 
-    @Test
-    public void testCanonicalNameInnerInnerClass() {
-        String name = ClassUtils.canonicalName( A.B.class );
-        assertEquals( "org.kie.workbench.common.services.backend.compiler.impl.classloader.MapClassLoaderTest.A.B",
-                      name );
-    }
-
-    @Test
-    public void testCanonicalNameArray() {
-        String name = ClassUtils.canonicalName( Object[].class );
-        assertEquals( "java.lang.Object[]",
-                      name );
-    }
-
-    @Test
-    public void testCanonicalNameMultiIndexArray() {
-        String name = ClassUtils.canonicalName( Object[][][].class );
-        assertEquals( "java.lang.Object[][][]",
-                      name );
-    }
-
-    @Test
-    public void testCanonicalNameMultiIndexArrayInnerClass() {
-        String name = ClassUtils.canonicalName( A.B[][][].class );
-        assertEquals( "org.kie.workbench.common.services.backend.compiler.impl.classloader.MapClassLoaderTest.A.B[][][]",
-                      name );
-    }
-
-    @Test
-    public void testCanonicalNameMultiIndexArrayPrimitives() {
-        String name = ClassUtils.canonicalName( long[][][].class );
-        assertEquals( "long[][][]",
-                      name );
-    }
-
-    public static class A {
-        public static class B {
-        }
     }
 
 }
