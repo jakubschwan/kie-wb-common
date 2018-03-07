5d4a3d1de33e0b115bb6e595f3788a03a9739da4
Jakub Schwan <jschwan@redhat.com>
2018-03-07 17:02:05 +0100

    CompilerClassloaderUtilsTest: Add test for filtering sub class paths

--- kie-wb-common-services/kie-wb-common-compiler/kie-wb-common-compiler-core/src/test/java/org/kie/workbench/common/services/backend/compiler/impl/classloader/CompilerClassloaderUtilsTest.java
+++ kie-wb-common-services/kie-wb-common-compiler/kie-wb-common-compiler-core/src/test/java/org/kie/workbench/common/services/backend/compiler/impl/classloader/CompilerClassloaderUtilsTest.java
@@ -90,16 +90,28 @@ public class CompilerClassloaderUtilsTest extends BaseCompilerTest {
 
     @Test
     public void filterPathClasses() {
-        List<String> targets = new ArrayList<>(3);
+        List<String> targets = new ArrayList<>(6);
         targets.add("/target/classes/org/kie/test/A.class");
         targets.add("/target/classes/io/akka/test/C.class");
         targets.add("/target/classes/com/acme/test/D.class");
-        targets.add("/target/classes/com/acme/test/D.class");
+        targets.add("/target/classes/com/acme/test/E.class");
         targets.add(mavenRepo.toAbsolutePath().toString() + "/junit/junit/4.12/junit.jar");
         targets.add(mavenRepo.toAbsolutePath().toString() + "/junit/junit/4.12/junit-4.12.jar");
 
         Set<String> orgKie = CompilerClassloaderUtils.filterPathClasses(targets, mavenRepo.toString());
         assertThat(orgKie).hasSize(4);
+    }
+
+    @Test
+    public void filterPathSubClasses() {
+        List<String> targets = new ArrayList<>(4);
+        targets.add("/target/classes/org/kie/test/A.class");
+        targets.add("/target/classes/org/kie/test/child/B.class");
+        targets.add("/target/classes/org/kie/test/child/son/C.class");
+        targets.add("/target/classes/org/kie-test/T.class");
+
+        Set<String> orgKie = CompilerClassloaderUtils.filterPathClasses(targets, mavenRepo.toString());
+        assertThat(orgKie).hasSize(4).contains("org.kie.test","org.kie.test.child","org.kie.test.child.son", "org.kie-test");
     }
 
     @Test
@@ -131,8 +143,7 @@ public class CompilerClassloaderUtilsTest extends BaseCompilerTest {
 
     @Test
     public void createClassloaderFromCpFiles() {
-        assertThat(!res.getDependencies().isEmpty()).isTrue();
-        assertThat(res.getDependencies().size()).isEqualTo(4);
+        assertThat(res.getDependencies()).hasSize(4);
         Optional<ClassLoader> classLoader = CompilerClassloaderUtils.createClassloaderFromStringDeps(res.getDependencies());
         assertThat(classLoader.isPresent()).isTrue();
         assertThat(classLoader.get()).isNotNull();
